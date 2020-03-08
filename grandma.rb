71c850731860adbe7c701ff4a45a39957f6315ee
Winifred Frierson <winifred.dykes@hotmail.com>
2020-03-08 05:20:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,7 +11,7 @@ def speak_to_grandma(phrase)
   elsif phrase == phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
   
-  else  return "NO, NOT SINCE 1938!"
+  
   
   #  "NO, NOT SINCE 1938!"
   end
